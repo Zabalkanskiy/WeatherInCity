0a43b15a3efa45fe786034cd1924981457011190
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-08-17 10:52:11 +0300

    Add WeatherApi retrofit call

---
+++ app/src/main/java/com/example/weatherincity/retrofit/RetrifitApiService.kt
@@ -0,0 +1,36 @@
+package com.example.weatherincity.retrofit
+
+import com.example.weatherincity.retrofit.data.WeatherData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+private const val WEATHER_URL ="https://api.weatherapi.com/v1/"
+
+object RetrifitApiService {
+    val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(WEATHER_URL)
+        .build()
+
+    fun weatherService(): WeatherApi = retrofit.create(WeatherApi::class.java)
+
+    fun loadingWeatherData(city: String): WeatherData?{
+        var weatherData: WeatherData? = null
+        CoroutineScope(Dispatchers.Main).launch {
+            var weatherData: WeatherData? = null
+           val job = CoroutineScope(Dispatchers.IO).launch {
+              weatherData =  weatherService().getWeather(city)
+            }
+            job.join()
+
+        }
+
+        return weatherData
+    }
+
+
+}
---
+++ app/src/main/java/com/example/weatherincity/retrofit/WeatherApi.kt
@@ -0,0 +1,13 @@
+package com.example.weatherincity.retrofit
+
+import com.example.weatherincity.retrofit.data.WeatherData
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface WeatherApi {
+    //need add your Api key!!!
+   // @GET("forecast.json?q={city}&days=5&key=07ffa400e8cb41f1b6b111917231408")
+    @GET("forecast.json?days=5&key=07ffa400e8cb41f1b6b111917231408")
+    suspend fun getWeather(@Query("q") city :String): WeatherData
+}
