2d07ffbec826206d02b4855a9cdf37fdef9d91dd
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-08-17 10:53:18 +0300

    MainActivity.kt

---
+++ app/src/main/java/com/example/weatherincity/model/ViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.weatherincity.model
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherincity.retrofit.RetrifitApiService
+import com.example.weatherincity.retrofit.data.Forecastday
+import com.example.weatherincity.retrofit.data.WeatherData
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class ViewModel : ViewModel(){
+
+    val state = mutableStateOf<WeatherData?>(null)
+    val mutableLiveData = MutableLiveData<WeatherData>()
+    val liveData: LiveData<WeatherData> = mutableLiveData
+
+    private  val  errorHandler = CoroutineExceptionHandler { _, exeption ->
+        exeption.printStackTrace()
+    }
+
+    fun getWeather(city: String){
+        viewModelScope.launch(Dispatchers.IO + errorHandler) {
+            val weatherData = RetrifitApiService.weatherService().getWeather(city)
+            withContext(Dispatchers.Main){
+               // state.value = weatherData
+                mutableLiveData.postValue(weatherData)
+            }
+        }
+    }
+    val retrofit = RetrifitApiService
+
+    fun getWeatherData(city: String): WeatherData?{
+       return retrofit.loadingWeatherData(city)
+
+
+    }
+
+    fun getFiveDayData(weatherData: WeatherData): List<Forecastday>{
+       return weatherData.forecast.forecastday
+    }
+}
